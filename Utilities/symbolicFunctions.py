a3bfe4511db9d7899538bb60fb9dd61b98313d4e
Markus J <mbjaasun@stud.ntnu.no>
2021-11-18 12:17:30 +0100

    Cleaning

--- Utilities/symbolicFunctions.py
+++ Utilities/symbolicFunctions.py
@@ -63,7 +63,58 @@ def Slist_maker(omega_mat, q_mat): #omega_mat and q_mat of type matrix with q_i 
         Slist[3:,i] = v_mat[:,i]
     return Slist
 
+def Js_maker(Slist, theta_list):
+    n_joints = Slist.shape[1]
+    Js = sp.zeros(6, n_joints)
 
+    for i in range(n_joints-1, -1, -1):
+        if i==0: # legger til denne for å få Js[0] = S_sb[0], står i boka
+            Js[:,i] = Slist[:,i]
+                    
+        else:
+            T = exp6(Slist[:,i-1], theta_list[i-1])
+
+            for j in range( i-2, -1, -1):
+                T = exp6(Slist[:,j], theta_list[j]) * T
+
+        Js[:,i] = Ad(T) * Slist[:,i]
+        
+
+    Js.simplify()
+    return Js
+
+def Jb_maker(Blist, theta_list):
+    n_joints = Blist.shape[1] - 1
+    Jb = sp.zeros(6, 6)
+    print(n_joints)
+    
+    Jb[:,n_joints] = Blist[:,n_joints] # Jb[n] = B[n]
+    
+    for i in range(n_joints-1, -1, -1):
+        T = sp.eye(4)#exp6(Blist[:,i], -theta_list[i])
+        print("i",i)
+        for j in range( i+1, n_joints+1):
+            T = sp.exp6(Blist[:,j], -theta_list[j]) * T
+            print("j",j)
+        Jb[:,i] = Ad(T) * Blist[:,i]
+        print("\n")
+    
+    return Jb
+
+def applyJointLim(jointLimits, thetas):
+    ''' Check if Inverse Kinematics solution (thetas) is within jointlimits\n 
+    PARAMETERS:
+    Jointlimits, numpy 2D array with lower and upper limits in deg\n
+    Jointangles, numpy 1D array in rad
+    RETURNS: Boolean true or false, if false a print message with the offending link is printed '''
+    jointLimits = np.deg2rad(jointLimits) 
+    thetas %= 2*np.pi #Post processing: all thetas in [0,2*pi)
+
+    for theta, i in enumerate(thetas):
+        if jointLimits[i][1] < theta < jointLimits[i][0]:
+            print("Joint number: ", i+1, "is not within the limits")
+            return False
+    return True
 
 
 #____DH-functions____
