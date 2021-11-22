4d00b47d0f35c444a1aca4fe5c4a11ed36cd5e1a
Adrian Gjertsen <adriangj@hotmail.no>
2021-11-22 19:18:12 +0100

    rengjort task 3-2

--- Utilities/symbolicFunctions.py
+++ Utilities/symbolicFunctions.py
@@ -94,28 +94,13 @@ def Jb_maker(Blist, theta_list):
         T = sp.eye(4)#exp6(Blist[:,i], -theta_list[i])
         print("i",i)
         for j in range( i+1, n_joints+1):
-            T = sp.exp6(Blist[:,j], -theta_list[j]) * T
+            T = exp6(Blist[:,j], -theta_list[j]) * T
             print("j",j)
         Jb[:,i] = Ad(T) * Blist[:,i]
         print("\n")
     
     return Jb
 
-jointLimits = np.array([[-180, 180], [-190, 45], [-120, 156], [-180, 180], [-90, 90], [-180, 180]]) #Assuming joint 5 has limits [-90, 90]
-def applyJointLim(jointLimits, thetas):
-    ''' Check if Inverse Kinematics solution (thetas) is within jointlimits\n 
-    PARAMETERS:
-    Jointlimits, numpy 2D array with lower and upper limits in deg\n
-    Jointangles, numpy 1D array in rad
-    RETURNS: Boolean true or false, if false a print message with the offending link is printed '''
-    jointLimits = np.deg2rad(jointLimits) 
-    thetas %= 2*np.pi #Post processing: all thetas in [0,2*pi)
-
-    for i, theta in enumerate(thetas):
-        if (jointLimits[i][1] < theta) | (theta < jointLimits[i][0]):
-            print("Joint number: {} is outside limits. Theta: {} | limits: {}".format(i+1, np.rad2deg(float(theta)), np.rad2deg(jointLimits[i])))
-            return False
-    return True
 
 
 #____DH-functions____
@@ -152,3 +137,108 @@ def ps_from_Tsd(T_sd):
     P_d = np.array([0,0,80,1])
     P_s = T_sd@P_d
     return P_s
+
+
+jointLimits = np.array([[-180, 180], [-190, 45], [-120, 156], [-180, 180], [-90, 90], [-180, 180]]) #Assuming joint 5 has limits [-90, 90]
+def apply_joint_lim(jointLimits, thetas):
+    ''' Check if Inverse Kinematics solution (thetas) is within jointlimits\n 
+    PARAMETERS:
+    Jointlimits, numpy 2D array with lower and upper limits in deg\n
+    Jointangles, numpy 1D array in rad
+    RETURNS: Boolean true or false, if false a print message with the offending link is printed '''
+    jointLimits = np.deg2rad(jointLimits) 
+    #thetas %= 2*np.pi #Post processing: all thetas in [0,2*pi)
+
+    #for theta, i in enumerate(thetas):
+    #    if jointLimits[i][1] < theta < jointLimits[i][0]:
+    #        print("Joint number: ", i+1, "is not within the limits")
+    #        return False
+    #return True
+    for i in range(0,len(thetas)):
+        if jointLimits[i,1] < thetas[i] or thetas[i] < jointLimits[i,0]:
+            print("Joint number: ", i+1, "is not within the limits")
+            return False
+    return True
+
+
+def agilus_theta_23(T_sd):
+    """
+    Calculates theta 2 and 3 of the agilus 6R robot
+    PARAMTERS:
+    T_sd: The desired end effector pose
+    RETURNS: floats, Thetas 2 and 3 for both elbow up and elbow down solutions.
+    """
+
+    Ps = ps_from_Tsd(T_sd)
+    P2 = np.array([Ps[0],Ps[1],Ps[2]-400]) # The same as Ps, but now relative to joint 2. Needed to do the following trigonometrics
+
+    # Define the edges of the constructed triangle:
+    a = np.sqrt(420**2+35**2)
+    c = 455
+    b = np.sqrt((np.sqrt(P2[0]**2+P2[1]**2)-25)**2 + P2[2]**2)
+
+    # Calculate the four angles needed:
+    psi = np.arccos(420/a)
+    phi = sp.atan2(P2[2], sp.sqrt(P2[0]**2 + P2[1]**2)-25)
+    alpha = np.arccos((b**2+c**2-a**2)/(2*b*c))
+    beta = np.arccos((a**2+c**2-b**2)/(2*a*c))
+
+    # Calculate the elbow up and elbow down solutions of theta2 and theta3
+    theta2_down =  -(phi - alpha)
+    theta3_down =  -(np.pi - beta - psi)
+    theta2_up = -(alpha + phi)
+    theta3_up = np.pi - (beta - psi)
+
+    return float(sp.N(theta2_up)), float(sp.N(theta3_up)), float(sp.N(theta2_down)), float(sp.N(theta3_down))
+
+
+
+def euler_nx_y_nx(R):
+    """
+    Calculates the Euler angles for rotations about (-x)y(-x)
+    PARAMETERS:
+    R: The desired rotation
+    RETURNS:
+    float, Three angles
+    """
+    theta_x1 = -sp.atan2(R[1,0], -R[2,0])
+    theta_y = sp.atan2(sp.sqrt(1-R[0,0]**2), R[0,0])
+    theta_x2 = -sp.atan2(R[0,1], R[0,2])
+
+    return float(sp.N(theta_x1)), float(sp.N(theta_y)), float(sp.N(theta_x2))
+
+
+def agilus_analytical_IK(Slist,M,T_sd):
+    """
+    Computes the analytical inverse kinematics of the Agilus 6R robot.
+    PARAMETERS:
+    Tsd: The desired end-effector pose
+    RETURNS: float arrays of joint values, elbow up and elbow down.
+    """
+    thetas_up = [0,0,0,0,0,0]
+    thetas_down = [0,0,0,0,0,0]
+    Ps = ps_from_Tsd(T_sd)
+
+    # Theta 1
+    thetas_up[0] = float(sp.N(-sp.atan2(Ps[1],Ps[0])))  # Minus sign since the axis of rotation is defined as -z.
+    thetas_down[0] = thetas_up[0]
+
+    # Thetas 2,3
+    thetas_up[1], thetas_up[2], thetas_down[1], thetas_down[2] = agilus_theta_23(T_sd)
+
+    # Thetas 4,5,6
+     # Elbow down:
+    T1 = exp6(Slist[:,0], -thetas_down[0])
+    T2 = exp6(Slist[:,1], -thetas_down[1])
+    T3 = exp6(Slist[:,2], -thetas_down[2])
+    R_down = (T3@T2@T1@T_sd@np.linalg.inv(M))  # The remaining rotation needed, defined in s
+    thetas_down[3], thetas_down[4], thetas_down[5] = euler_nx_y_nx(R_down)
+
+     # Elbow up:
+    T1 = exp6(Slist[:,0], -thetas_up[0])
+    T2 = exp6(Slist[:,1], -thetas_up[1])
+    T3 = exp6(Slist[:,2], -thetas_up[2])
+    R_up = (T3@T2@T1@T_sd@np.linalg.inv(M))
+    thetas_up[3], thetas_up[4], thetas_up[5] = euler_nx_y_nx(R_up)
+
+    return thetas_up, thetas_down
